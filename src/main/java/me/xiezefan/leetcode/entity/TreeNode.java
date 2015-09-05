df0cedb8a8baf42c9f680531ca171f1a612e6443
xiezefan <xiezefan@126.com>
2015-09-05 14:51:38 +0800

    add TreeNode

---
+++ src/main/java/me/xiezefan/leetcode/entity/TreeNode.java
@@ -0,0 +1,12 @@
+package me.xiezefan.leetcode.entity;
+
+/**
+ * Common Tree node
+ * Created by xiezefan on 15/9/5.
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
+}
